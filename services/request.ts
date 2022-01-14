a77d11aa1bad68380e54bf9e8afaf64be6dc7458
Maxence Raballand <47566657+maxencerb@users.noreply.github.com>
2022-01-14 15:25:59 +0100

    implemented Base64 algorithm instead of btoa

--- services/request.ts
+++ services/request.ts
@@ -1,5 +1,6 @@
 import { defaultHeaders } from "@services/utils/request";
 import { Request, AppRequestType, AppResponse } from "@models/request";
+import { encode } from "./utils/base64";
 
 const localRequest = async (request: Request) => {
     const headers: any = {
@@ -21,7 +22,7 @@ const fromRequestToRawRequest = (request: AppRequestType): string => {
 
     if ((request.authInfo.type === 'Basic' && request.authInfo.username && request.authInfo.password) || ((request.authInfo.type === 'Bearer' || request.authInfo.type === 'Custom') && request.authInfo.token)) {
         const type = request.authInfo.type !== 'Custom' ? request.authInfo.type : '';
-        const token = request.authInfo.type === 'Basic' ? btoa(`${request.authInfo.username}:${request.authInfo.password}`) : request.authInfo.token;
+        const token = request.authInfo.type === 'Basic' ? encode(`${request.authInfo.username}:${request.authInfo.password}`) : request.authInfo.token;
         content += `Authorization: ${type} ${token}\r\n`;
     }
     request.headers.forEach((header) => {
